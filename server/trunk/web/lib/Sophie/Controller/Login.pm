6dd5322082e2d4ba8b266d49cc32fae98dbf9db9
nanardon <nanardon@824b5022-98b7-4ee1-a271-9ce0ea818ce6>
2010-11-24 09:09:18 +0000

    - add login controller

---
+++ server/trunk/web/lib/Sophie/Controller/Login.pm
@@ -0,0 +1,58 @@
+package Sophie::Controller::Login;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Sophie::Controller::Login - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) :XMLRPCPath('/login') {
+    my ( $self, $c, $login, $password ) = @_;
+
+    if ($c->authenticate({
+            id => $login || $c->req->param('username'),
+            password => $password || $c->req->param('password'),
+        }
+    )) {
+        if ($c->req->xmlrpc->is_xmlrpc_request) {
+            $c->stash->{xmlrpc} = 'sophie_session=' . $c->sessionid;
+        } else {
+            $c->res->redirect('/');
+        }
+    } else {
+        if ($c->req->xmlrpc->is_xmlrpc_request) {
+            $c->error('invalid login / password');
+        }
+    }
+}
+
+
+=head1 AUTHOR
+
+Olivier Thauvin
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
