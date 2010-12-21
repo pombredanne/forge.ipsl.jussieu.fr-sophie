9e05a9131d12e85ffbdcbf42449c0800a430f353
nanardon <nanardon@824b5022-98b7-4ee1-a271-9ce0ea818ce6>
2010-12-21 13:53:57 +0000

    - tag maintainers source as updated

--- server/trunk/web/lib/Sophie/Base/Result/MaintSources.pm
+++ server/trunk/web/lib/Sophie/Base/Result/MaintSources.pm
@@ -5,7 +5,7 @@ use warnings;
 use base qw(DBIx::Class::Core);
 
 __PACKAGE__->table('maint_sources');
-__PACKAGE__->add_columns(qw/pkey url label accessor/);
+__PACKAGE__->add_columns(qw/pkey url label accessor lastupdated/);
 __PACKAGE__->set_primary_key('pkey');
 #__PACKAGE__->belongs_to(Distribution => 'Sophie::Base::Result::Distribution', 'distributions');
 __PACKAGE__->has_many(MaintRpm => 'Sophie::Base::Result::MaintRpm', 'sources');
