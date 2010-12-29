8f9f4ca86fa6c35804f3caa7e5e99859d2803d59
nanardon <nanardon@824b5022-98b7-4ee1-a271-9ce0ea818ce6>
2010-12-29 14:02:37 +0000

    - replace useless controller by 'Compat' controller to handle previous urls

---
+++ server/trunk/web/lib/Sophie/Controller/Compat.pm
@@ -0,0 +1,63 @@
+package Sophie::Controller::Compat;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Sophie::Controller::Compat - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+sub viewrpm :Path('/viewrpm') :Args(0) {
+    my ($self, $c) = @_;
+    $c->res->redirect($c->uri_for('/rpms'));
+}
+
+sub viewrpms :Path('/viewrpm') :Args {
+    my ($self, $c, $pkgid, @args) = @_;
+
+    $c->res->redirect($c->uri_for('/rpms', $pkgid, @args));
+}
+
+sub rpm :Path('/rpm') :Args(2) {
+    my ($self, $c, $dist, $rpm) = @_;
+    
+    my @args = split(',', $dist);
+    if (@args == 2) {
+        unshift(@args, '');
+    }
+    $c->res->redirect($c->uri_for('/distrib', @args, 'rpms', $rpm));
+}
+
+sub srpm :Path('/srpm') :Args(2) {
+    my ($self, $c, $dist, $rpm) = @_;
+    
+    my @args = split(',', $dist);
+    if (@args == 2) {
+        unshift(@args, '');
+    }
+    $c->res->redirect($c->uri_for('/distrib', @args, 'srpms', $rpm));
+}
+
+=head1 AUTHOR
+
+Olivier Thauvin
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
--- server/trunk/web/t/controller_Compat.t
+++ server/trunk/web/t/controller_Compat.t
@@ -3,7 +3,7 @@ use warnings;
 use Test::More;
 
 BEGIN { use_ok 'Catalyst::Test', 'Sophie' }
-BEGIN { use_ok 'Sophie::Controller::Viewrpm' }
+BEGIN { use_ok 'Sophie::Controller::Compat' }
 
 ok( request('/viewrpm')->is_redirect, 'Request should succeed' );
 ok( request('/viewrpm/azerty')->is_redirect, 'Request should succeed' );
