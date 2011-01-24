6deaeaf24223f67c717021a49e25b33db5d59349
nanardon <nanardon@824b5022-98b7-4ee1-a271-9ce0ea818ce6>
2011-01-24 17:10:33 +0000

    - add missing files

---
+++ server/trunk/web/lib/Sophie/Scan/RpmParser.pm
@@ -0,0 +1,38 @@
+package Sophie::Scan::RpmParser;
+
+use strict;
+use warnings;
+use File::Temp;
+use File::Copy;
+use Archive::Cpio;
+
+sub new {
+    my ($class, $base) = @_;
+
+    bless { _base => $base }, $class;
+}
+
+sub base { $_[0]->{_base} }
+
+sub traverse_cpio {
+    my ($self, $rpm, $sub) = @_;
+    my $scan = $self->base;
+
+    my $tmp = File::Temp->new( UNLINK => 1, SUFFIX => '.cpio' );
+    unlink($tmp->filename);
+    if (open(my $cpioh, "rpm2cpio " . quotemeta($rpm) . " |")) {
+        File::Copy::copy($cpioh, $tmp);
+        close($cpioh);
+    } else {
+        return 0;
+    }
+    
+    seek($tmp, 0, 0);
+    my $cpio = Archive::Cpio->new();
+    eval {
+        $cpio->read_with_handler($tmp, $sub);
+    };
+    $tmp = undef;
+}
+
+1;
