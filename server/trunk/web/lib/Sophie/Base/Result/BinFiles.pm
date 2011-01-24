86091833e16751e8fd6e195195853c4e25950f69
nanardon <nanardon@824b5022-98b7-4ee1-a271-9ce0ea818ce6>
2011-01-24 17:14:00 +0000

    - add BinFiles resultset

---
+++ server/trunk/web/lib/Sophie/Base/Result/BinFiles.pm
@@ -0,0 +1,15 @@
+package Sophie::Base::Result::BinFiles;
+
+use strict;
+use warnings;
+use base qw(DBIx::Class::Core);
+
+__PACKAGE__->table('binfiles');
+__PACKAGE__->add_columns(qw/dirnamekey pkgid count basename md5 user group linkto
+    mode fflags size class color vflags mtime nlink has_content/);
+__PACKAGE__->set_primary_key(qw/pkgid count/);
+__PACKAGE__->belongs_to(Rpms => 'Sophie::Base::Result::Rpms', 'pkgid');
+__PACKAGE__->belongs_to(Directories => 'Sophie::Base::Result::Directories', 'dirnamekey');
+
+
+1;
