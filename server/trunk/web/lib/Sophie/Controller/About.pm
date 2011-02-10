dd0471096ec851d9de6d6e500bd1f9d38deefb9d
misc <misc@824b5022-98b7-4ee1-a271-9ce0ea818ce6>
2011-02-10 00:22:09 +0000

    add a about page, with link to the code and bugtracker

---
+++ server/trunk/web/lib/Sophie/Controller/About.pm
@@ -0,0 +1,43 @@
+package Sophie::Controller::About;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Sophie::Controller::About - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+}
+
+
+=head1 AUTHOR
+
+Michael Scherer
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
